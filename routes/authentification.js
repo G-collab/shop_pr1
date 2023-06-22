d846c618197f17afa4f1857bc1718c7d8ed71b44
G-collab <galiatinvova@gmail.com>
2023-06-22 21:18:43 +0300

    created Models and authentification. new files in routes

---
+++ routes/authentification.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const user = require("../Models/User")
+//reg
+router.post("/register", (req,res)=>{
+    const newUser = new User
+})
+module.exports = router
